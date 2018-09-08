bbf08526744ea691a2106e0acd0bc2e09ed84f08
noamha <noamhasin@gmail.com>
2018-09-08 18:06:42 +0300

    Ad more asanas and fix practice flow

--- asanas.py
+++ asanas.py
@@ -1,4 +1,13 @@
-from soundstools import playSound, playWinbip, time, save_text, count_duration
+from soundstools import playSound, playWinbip, time, save_text, count_duration, get_sound_file_path
+from enum import Enum, auto
+from pathlib import Path
+import datetime
+
+class Breath(Enum):
+    EXHALE = auto()
+    INHALE = auto()
+    RIGHT = auto()
+    LEFT = auto()
 
 
 # Class Viniasa 
@@ -6,24 +15,32 @@ class Viniasa:
     
     viniasaNum = 0
     viniasa_breath = 3
+    breath_order = Breath.INHALE
 
-    def __init__(self, num, viniasa_breath=3):
+    def __init__(self, num, viniasa_breath=3, breath_order=Breath.INHALE):
         self.viniasaNum = num
         self.viniasa_breath = viniasa_breath
+        self.breath_order = breath_order
 
     def execute(self):
         print("Execute ", "Viniasa")
         i = 1
         
         while i <= self.viniasaNum:
-   #         time.clock(self.viniasa_breath)
-            if i%2 > 0: 
-                playSound("Inhale")
+            # time.clock(self.viniasa_breath)
+            if i % 2 > 0:
+                if self.breath_order == Breath.INHALE:
+                    playSound("Inhale")
+                else:
+                    playSound("Exhale")
             else:
-                playSound("Exhale")
-            i = i + 1   
+                if self.breath_order == Breath.INHALE:
+                    playSound("Exhale")
+                else:
+                    playSound("Inhale")
+            i = i + 1
             count_duration(self.viniasa_breath)
-            #print (time.clock(), " seconds process time")
+            # print (time.clock(), " seconds process time")
 
 # Class Asana
 class Asana:
@@ -32,26 +49,37 @@ class Asana:
     Description = ""
     pre_viniasaNum = 3
     post_viniasaNum = 3
+    breath_order = Breath.INHALE
+    start_with_side = Breath.RIGHT
 
     asana_duration = 30
     BreathSecs = 0
 
-    def __init__(self,duration):
+    def __init__(self, duration):
         self.asana_duration = duration
 
     def set_breath_count(self, secs):
         print("Breath Count")
         self.BreathSecs = secs
 
-    def set_pre_viniasa_num(self, pre_viniasaNum=3):
+    def set_pre_viniasa_num(self, pre_viniasaNum=2):
         self.pre_viniasaNum = pre_viniasaNum
 
-    def set_post_viniasa_num(self, post_viniasaNum=3):
+    def set_post_viniasa_num(self, post_viniasaNum=2):
         self.post_viniasaNum = post_viniasaNum
+
+    def set_breath_order(self, breath_order):
+        self.breath_order = breath_order
+
+    def set_start_with_side(self, side=Breath.RIGHT):
+        self.start_with_side = side
+
+    def get_start_side(self):
+        return self.start_with_side
 
 
 class Practice(Asana):
-    
+
     def __init__(self, name="practice", description="Asana", duration=0):
         
         Asana.__init__(self, duration)
@@ -60,6 +88,7 @@ class Practice(Asana):
         self.asana_duration = duration
         self.pre_viniasa = Viniasa(self.pre_viniasaNum)
         self.post_viniasa = Viniasa(self.post_viniasaNum)
+        self.start_prectice_time = time.clock()
 
     def __str__(self):
         return self.Name
@@ -70,28 +99,44 @@ class Practice(Asana):
     def get_description(self):
         return self.Description
         
-    def run_viniasa(self, num):
-        viniasa = Viniasa(num, self.BreathSecs)
+    def run_viniasa(self, num, start_with=Breath.INHALE):
+        viniasa = Viniasa(num, self.BreathSecs, start_with)
         viniasa.execute()
         
-    def create_voice_name(self):
-        save_text(self.get_description(), self.Name)
-        
+    def create_voice_name(self, force=False):
+        if force:
+            save_text(self.get_description(), self.Name)
+        else:
+            my_file = Path(get_sound_file_path() + self.Name + ".mp3")
+            if not my_file.is_file():
+                save_text(self.get_description(), self.Name)
+
     def say_my_name(self):
         playSound(self.Name)
         time.sleep(3)
-              
+
+
     def execute(self):
-        
-        print ("Execute ", self.Name)
+
+        self.say_practice_time()
+        print("Execute ", self.Name)
         time.clock()
-        self.run_viniasa(self.pre_viniasaNum)
+        self.run_viniasa(self.pre_viniasaNum, self.breath_order)
 #        playSound("Inhale")
         count_duration(self.asana_duration)
         #print (time.clock(),self.Name, " seconds process time (Practice)")
         self.run_viniasa(self.post_viniasaNum)
  #       playSound("Exhale")
         #print (time.clock(), "seconds process time")
+
+
+def say_practice_time():
+        start_practice_hour = datetime.datetime.time(datetime.datetime.now()).hour
+        start_practice_minute = datetime.datetime.time(datetime.datetime.now()).minute
+
+        save_text("Time is: " + str(start_practice_hour) + " and " + str(start_practice_minute) + " minutes","currentTime")
+        playSound("currentTime")
+        time.sleep(3)
 
 def general_asana_count(numOfAsanas):
     
@@ -106,210 +151,235 @@ def general_asana_count(numOfAsanas):
         playSound("Exhale")
         #print (time.clock(), "seconds process time")
 
-def asanaIteration(asana,num):
-    
+
+def asana_iteration_by_num(asana, num):
+
     i = 1
     while i <= num:
         if num > 1:
             playSound(str(i))
             time.sleep(3)
-        print("Asana:" , str(i))
+        print("Asana:", str(i))
         asana.execute()
         i = i + 1
 
-    
+
+def asana_iteration_by_sides(asana, num):
+    i = 1
+    while i <= num:
+        if num > 1:
+            # print('debug', i, asana.get_start_side().value)
+            if asana.get_start_side() is Breath.RIGHT:
+                if i%2:
+                    playSound('Right')
+                else:
+                    playSound('Left')
+            elif asana.get_start_side() is Breath.LEFT:
+                if i%2:
+                    playSound('Left')
+                else:
+                    playSound('Right')
+            time.sleep(3)
+        print("Asana:", str(i))
+        asana.execute()
+        i = i + 1
+
+
 def sunSalutationA(instance_num):
 
-    Sun1 = Practice("SunSalutationA", "Surya Namaskara A",30)
-    Sun1.create_voice_name()
-    Sun1.say_my_name()
+    my_practice = Practice("SunSalutationA", "Surya Namaskara A", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
     playSound(str(instance_num))
     playSound("Times")
-    Sun1.set_breath_count(4)    
-    Sun1.set_pre_viniasa_num(6)
-    Sun1.set_post_viniasa_num(4)
-    asanaIteration(Sun1,instance_num)
+    my_practice.set_breath_count(4)    
+    my_practice.set_pre_viniasa_num(6)
+    my_practice.set_post_viniasa_num(4)
+    asana_iteration_by_num(my_practice, instance_num)
 
 
 def sunSalutationB(instance_num):
-    Sun2 = Practice("SunSalutationB", "Surya Namaskara B",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(4)
-    Sun2.set_pre_viniasa_num(14)
-    Sun2.set_post_viniasa_num(4)
-    asanaIteration(Sun2,instance_num)
+
+    my_practice = Practice("SunSalutationB", "Surya Namaskara B", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(4)
+    my_practice.set_pre_viniasa_num(14)
+    my_practice.set_post_viniasa_num(4)
+    asana_iteration_by_num(my_practice, instance_num)
 
 def padangusthasana(instance_num):
-    Sun2 = Practice("Padangusthasana", "Pada angoostha ausana",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(4)
-    Sun2.set_post_viniasa_num(2)
-    asanaIteration(Sun2,instance_num)
+
+    my_practice = Practice("Padangusthasana", "Pada angoostha ausana", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(4)
+    my_practice.set_post_viniasa_num(2)
+    asana_iteration_by_num(my_practice, instance_num)
 
 def padanhastaasana(instance_num):
     
-    Sun2 = Practice("Padanhastaasana", "Pada hasta ausana ",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("Padanhastaasana", "Pada hasta ausana ", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(3)
+    my_practice.set_post_viniasa_num(4)
+    my_practice.set_breath_order(Breath.EXHALE)
+    asana_iteration_by_num(my_practice, instance_num)
 
 
-def utthitatrikonasanaA(instance_num):
+def utthitatrikonasana(instance_num):
 
-    Sun2 = Practice("UtthitatrikonasanA", "ootthita treekona ausana  a",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)   
-    asanaIteration(Sun2,instance_num)
-
-
-def utthitatrikonasanaB(instance_num):
-
-    Sun2 = Practice("UtthitatrikonasanaB", "ootthita treekona ausana  b",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)   
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("Utthitatrikonasan", "ootthita treekona ausana.", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(2)
+    my_practice.set_post_viniasa_num(0)
+    asana_iteration_by_sides(my_practice, instance_num)
 
 
-def utthitatrikonasanaC(instance_num):
+def parivrittatrikonasana(instance_num):
 
-    Sun2 = Practice("UtthitatrikonasanaC", "ootthita treekona ausana  c",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)   
-    asanaIteration(Sun2,instance_num)
-    
+    my_practice = Practice("Parivrittatrikonasana", "parivritta treekona ausana.", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(2)
+    my_practice.set_post_viniasa_num(0)
+    my_practice.set_start_with_side(Breath.RIGHT)
+    asana_iteration_by_sides(my_practice, instance_num)
 
-def utthitatrikonasanaD(instance_num):
-    Sun2 = Practice("UtthitatrikonasanaD", "ootthita treekona ausana  d",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)   
-    asanaIteration(Sun2,instance_num)
 
-    
 def utthitaparsvakonasana(instance_num):
-    Sun2 = Practice("utthitaparsvakonasana", "ootthita parshvakona ausana ",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(0)   
-    asanaIteration(Sun2,instance_num)    
+    my_practice = Practice("utthitaparsvakonasana", "ootthita parshvakona ausana.", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(2)
+    my_practice.set_post_viniasa_num(0)
+    asana_iteration_by_sides(my_practice, instance_num)
 
 
 def parivrittaparsvakonasana(instance_num):
-    Sun2 = Practice("utthitaparsvakonasana", "Parivritta parshvakona ausana ",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)   
-    asanaIteration(Sun2,instance_num)    
+    my_practice = Practice("utthitaparsvakonasana", "Parivritta parshvakona ausana ", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(2)
+    my_practice.set_post_viniasa_num(0)
+    asana_iteration_by_sides(my_practice, instance_num)
 
 
 def prasaritapadottanasanaA(instance_num):
-    Sun2 = Practice("prasaritapadottanasanaA", "prasarita paadottana ausana  a",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)   
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("prasaritapadottanasanaA", "prasarita paadottana ausana  a", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(4)
+    my_practice.set_post_viniasa_num(2)   
+    asana_iteration_by_num(my_practice, instance_num)
 
 
 def prasaritapadottanasanaB(instance_num):
-    Sun2 = Practice("prasaritapadottanasanaB", "prasarita paadottana ausana  b",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)   
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("prasaritapadottanasanaB", "prasarita paadottana ausana  b", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(4)
+    my_practice.set_post_viniasa_num(2)   
+    asana_iteration_by_num(my_practice, instance_num)
     
 
 def prasaritapadottanasanaC(instance_num):
-    Sun2 = Practice("prasaritapadottanasanaC", "prasarita paadottana ausana  c",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)   
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("prasaritapadottanasanaC", "prasarita paadottana ausana  c", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(4)
+    my_practice.set_post_viniasa_num(2)   
+    asana_iteration_by_num(my_practice, instance_num)
     
 
 def prasaritapadottanasanaD(instance_num):
-    Sun2 = Practice("prasaritapadottanasanaD", "prasarita paadottana ausana  d",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)   
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("prasaritapadottanasanaD", "prasarita paadottana ausana  d", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(4)
+    my_practice.set_post_viniasa_num(2)   
+    asana_iteration_by_num(my_practice, instance_num)
 
 
 def parsvotanasana(instance_num):
-    Sun2 = Practice("Parsvotanasana", "Parshva oo tana ausana",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(2)   
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("Parsvotanasana", "Parshva oo tana ausana", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(4)
+    my_practice.set_post_viniasa_num(2)
+    asana_iteration_by_sides(my_practice, instance_num)
 
 
 def utthitahastapadangusthasanaA(instance_num):
-    Sun2 = Practice("utthitahastapadangusthasanaA", "ootthitaa haasta paada aangoostha ausana  a",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(1)   
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("utthitahastapadangusthasanaA", "uutthitaa haasta paada aangoostha ausana  a", 20)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(2)
+    my_practice.set_post_viniasa_num(2)
+    asana_iteration_by_num(my_practice, instance_num)
 
 
 def utthitahastapadangusthasanaB(instance_num):
-    Sun2 = Practice("utthitahastapadangusthasanaB", "ootthitaa haasta paada aangoostha ausana  b",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(1)   
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("utthitahastapadangusthasanaB", "uutthitaa haasta paada aangoostha ausana  b", 20)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(0)
+    my_practice.set_post_viniasa_num(2)
+    asana_iteration_by_num(my_practice, instance_num)
     
 
 def utthitahastapadangusthasanaC(instance_num):
-    Sun2 = Practice("utthitahastapadangusthasanaC", "ootthitaa haasta paada aangoostha ausana  c",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(1)   
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("utthitahastapadangusthasanaC", "uutthitaa haasta paada aangoostha ausana  c", 20)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(0)
+    my_practice.set_post_viniasa_num(2)
+    asana_iteration_by_num(my_practice, instance_num)
     
 
 def utthitahastapadangusthasanaD(instance_num):
-    Sun2 = Practice("utthitahastapadangusthasanaD", "ootthitaa haasta paada aangoostha ausana  d",30)
-    Sun2.create_voice_name()
-    Sun2.say_my_name()
-    Sun2.set_breath_count(3)
-    Sun2.set_pre_viniasa_num(2)
-    Sun2.set_post_viniasa_num(1)   
-    asanaIteration(Sun2,instance_num)
+    my_practice = Practice("utthitahastapadangusthasanaD", "uutthitaa haasta paada aangoostha ausana  d", 20)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(0)
+    my_practice.set_post_viniasa_num(2)
+    asana_iteration_by_num(my_practice, instance_num)
+
+
+def ardhabaddhapadmottanasana(instance_num):
+    my_practice = Practice("ardhabaddhapadmottanasana", "ardha baddha padmottana ausana", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(4)
+    my_practice.set_post_viniasa_num(2)
+    asana_iteration_by_sides(my_practice, instance_num)
+
+
+def utkatasana(instance_num):
+    my_practice = Practice("utkatasana", "uutkata ausana", 30)
+    my_practice.create_voice_name()
+    my_practice.say_my_name()
+    my_practice.set_breath_count(3)
+    my_practice.set_pre_viniasa_num(6)
+    my_practice.set_post_viniasa_num(4)
+    asana_iteration_by_sides(my_practice, instance_num)
 
 
--- soundstools.py
+++ soundstools.py
@@ -11,41 +11,41 @@ duration_soft = 100  # Set Duration To 1000 ms == 1 second
 output_voices = "voices"
 
 
+def get_sound_file_path():
+    return output_voices + "\\"
+
+
 def playSound(filename):
     mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
-    mixer.music.load(output_voices + "\\" + str(filename+".mp3"))
+    mixer.music.load(get_sound_file_path() + str(filename+".mp3"))
     mixer.music.play()
 
 
 def save_text(mytext,filename):
     tts = gTTS(text=mytext, lang='en')
-    tts.save(output_voices + "\\" + filename + ".mp3")
-    print("Creating ", filename , "for: ",mytext)
+    tts.save(get_sound_file_path() + filename + ".mp3")
+    print("Creating ", filename, "for: ", mytext)
     sys.stdout.flush()
     
 
 def createSounds():
     
     i = 1
-    while (i <= 30):
-        save_text(str(i) + " Seconds.",str(i)+"Secs")
+    while i <= 30:
+        save_text(str(i) + " Seconds.", str(i)+"Secs")
         i = i + 1
     i = 1
-    while (i <= 10):
-        save_text(str(i),str(i))
+    while i <= 10:
+        save_text(str(i), str(i))
         i = i + 1
 
-    save_text('Inhale','Inhale')
-    save_text('Exhale','Exhale')
-    save_text('Breathe for the next','takeBreath')
-    save_text('seconds.','seconds')
-    save_text('Lets start with Sun Salutation A','SunSalutationA')
-    save_text('Sun Salutation B','SunSalutationB')
-    save_text("Pada angoostha asana","Padangusthasana")
-    save_text("Pada hasta asana","Padanhastaasana")
-    save_text("Utthita trikona asana A","UtthitatrikonasanaA")
-    save_text("Parsvo Utana Asana","Parsvotanasana")
-    save_text('Times','Times')   
+    save_text('Right', 'Right')
+    save_text('Left', 'Left')
+    save_text('Inhale', 'Inhale')
+    save_text('Exhale', 'Exhale')
+    save_text('Breathe for the next', 'takeBreath')
+    save_text('seconds.', 'seconds')
+    save_text('Times', 'Times')
 
 
 def count_duration(secs):
@@ -55,14 +55,24 @@ def count_duration(secs):
         playSound('takeBreath')
         time.sleep(2)
         playSound(str(secs))        
-        secs = secs -2
+        secs = secs - 2
     
     while i < secs:
        time.sleep(1)
        i = i + 1
        sys.stdout.write('.')
        sys.stdout.flush()
-    print (" Finished " , i ," Secs")
+    print(" Finished ", i, " Secs")
 
 def playWinbip():
     winsound.Beep(frequency_soft, duration_soft)
+
+
+# stuff to run always here such as class/def
+def main():
+    createSounds()
+
+
+if __name__ == "__main__":
+    main()
+
